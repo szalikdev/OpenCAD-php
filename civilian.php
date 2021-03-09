30c6acfed31788aa25bc37afc39e7264abdf3d6b
szalik.dev <jakso215@wp.pl>
2021-03-09 09:49:09 +0100

    another try

--- civilian.php
+++ civilian.php
@@ -544,7 +544,7 @@ This program comes with ABSOLUTELY NO WARRANTY; Use at your own risk.
 								<label class="col-lg-2 control-label">Drivers License Status</label>
 								<div class="col-lg-10">
 									<select name="civDLStatus" class="form-control selectpicker civDLStatus_picker" id="civBuildReq" title="Select Status" required>
-										<?php getDataSetColumn($table = "ncic_names", $data = "dl_status"); ?>
+										<?php getDataSetColumn($table = "ncic_names", $data = "dl_status", $leadTrim = 24, $followTrim = 34); ?>
 									</select>
 								</div>
 								
